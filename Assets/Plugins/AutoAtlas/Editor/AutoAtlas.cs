d067d3822d1d2808e9d52a8c966f79f0d352142f
ivarus <ivarbjorling@hotmail.com>
2019-07-06 16:51:03 +0200

    init commit

---
+++ Assets/Plugins/AutoAtlas/Editor/AutoAtlas.cs
@@ -0,0 +1,25 @@
+﻿#if UNITY_EDITOR
+namespace AutoAtlas.Editor
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEditor;
+
+    [InitializeOnLoad]
+    public static class AutoAtlas
+    {
+        static AutoAtlas()
+        {
+        }
+
+        [MenuItem("AutoAtlas/Log")]
+        private static void Log()
+        {
+            Debug.Log("AutoAtlas.Log");
+        }
+    }
+}
+
+#endif
---
+++ README.md
@@ -0,0 +1,3 @@
+# Unity Auto Atlas Plugin
+
+This plugin auto generates sprite atlases.
